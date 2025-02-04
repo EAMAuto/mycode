129a8f6f717fe1f591d8a121242b7f8c1c678992
EAMAuto <erikasamuniz@hotmail.com>
2025-02-04 19:58:46 +0000

    backing up work

---
+++ custom_functions/ghostbusters_functions.py
@@ -0,0 +1,9 @@
+#!usr/bin/env python3
+
+def ghost(ghost, location = "New York City"):
+    print(f"{ghost} sighted at {location}! Who you gonna call?")
+    # any lines indented under a defined function
+    # a part of that function!
+
+ghost("Slimer")
+ghost("Stay Puft", "London")
