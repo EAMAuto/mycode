db984a98f7547017b6c61acb0a6b6de613deff7f
EAMAuto <erikasamuniz@hotmail.com>
2025-02-05 23:30:17 +0000

    backing up work

---
+++ Spidey_dictionary.py
@@ -0,0 +1,7 @@
+spidey= {"full name" : "Peter Benjaminn Parker", 
+         "species"   : "Human mutate" ,
+         "origin"    : "Queens, New York City"}
+
+spidey["slogan"] = "With great power comes great responsibility!"
+
+print(spidey)
