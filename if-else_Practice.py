d75fc16f8aef18ac36b51632aa3768735f589173
EAMAuto <erikasamuniz@hotmail.com>
2025-02-10 16:50:14 +0000

    backing up my work

---
+++ if-else_Practice.py
@@ -0,0 +1,12 @@
+pocket = []
+doorknob = ["keys"]
+fridge = ["keys"]
+
+if "keys" in pocket:
+    print ("Grab our keys and go!")
+if "keys" in fridge:
+    print("Grab our keys out of the fridge and go!")
+if "keys" in doorknob:
+    print ("How embarrassing! Grab our keys and go!")
+else: 
+    print("Guess I'll call a Lyft!")
