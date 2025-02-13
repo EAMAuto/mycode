22612a31a58d613618546b8f7861997c2f987d5b
EAMAuto <erikasamuniz@hotmail.com>
2025-02-13 02:58:13 +0000

    backing up my work

--- guessing_game.py
+++ guessing_game.py
@@ -4,19 +4,35 @@ import random
 
 secret_number = random.randint(1,10)
 
-guess = int(input ("Guess the number between 1 and 10: "))
 
-if guess < 1 or guess > 10:
-    print("Invalid guess. Please enter a number between 1 and 10.")
 
-elif guess == secret_number:
-    print("You guessed it! Great job")
 
-elif guess > secret_number:
-    print("Too High!")
+#WE WANT THE USER TO HAVE 3 ATTEMPTS!
+
+attempts = 0
+
+print(f"CHEATMODE: {secret_number}")
+
+while attempts < 3:
+    guess = input("Guess the number between 1 and 10: ")
+
+    if guess.isdigit():
+        guess = int(guess)
+    else:
+        print("Invalid guess. Choose a numeral!")
+        continue
+    if guess < 1 or guess > 10:
+        print("Invalid guess. Please enter a number between 1 and 10.")
+
+    elif guess == secret_number:
+        print("You guessed it! Great job")
+        break
+    elif guess > secret_number:
+        print("Too High!")
     
-elif guess < secret_number:
-    print ("Too Low!")
-
+    elif guess < secret_number:
+        print ("Too Low!")
+    
+    attempts +=1
 
 
---
+++ while_loops.py
@@ -0,0 +1,15 @@
+# WHILE LOOPS!
+
+x = 0
+
+while x<10:
+    x += 1
+    print("Hooray for x!")
+    print("Going to be repeated by the while loop!")
+    if x== 5:
+        print("BOO  TO X!")
+        break
+
+print("The while loop is over!")
+
+
