a4c0208781d2445adf86db40381ae88333fd4ece
EAMauto <erikasamuniz@hotmail.com>
2025-02-22 20:33:20 +0000

    Backing up module 1 work

---
+++ udemy_labs/module1_numericalreprisentation.py
@@ -0,0 +1,30 @@
+#There are two main numerical types integers and floats 
+
+#INTEGERS being represented as numbers without quoatations 
+#FLOATS being represented as numbers which include a decimal also without quotations
+
+# Now beginning in python3.6 you can use Underscores in numbers (_) this does not change the value of the number just the readability
+
+#for example 12000300 vs 12_000_300 are both equal
+
+#when working with floats a scientific notation can be used when using very big or very small numbers to avoid writing too many zero's
+
+# for example 
+#3e4 = 3E4 - 3 *10000 = 30000
+#3e-4 = 3E-4 = 3 *1/10000= 0.0003
+
+# when you use the print function python will automatically use the scientific notation
+# for example the output of print(0.000000000000000000000000003) would be 3e-27(roughly, I didnt count the 0 but you get the point)
+
+# finally integer numbers can be represented in 2 other ways as octal #'s and as hexidecimal numbers
+
+# Octal #'s 
+# start with 0O or 0o the number is an octal value
+# the #'s that follow this type of prefic must from the range 0-7 only, digits 8 & 9 are not allowed
+# for example print(0o123) == 83 in standard decimal notation
+
+# Hexadecimal numbers 
+# start with 0X or 0x
+#for example print(0x123) = 291 in the standard decimal notation
+
+#octal and hexadecimal #'s are rarely used in high level programming simply remember that octal #'s start with 0o or 0O and hexadecimal #'s begin with 0X or 0x
---
+++ udemy_labs/module1_operators.py
@@ -0,0 +1,30 @@
+# Operators in python are special symbols we use to perform mathematical operations 
+# + add 
+# - subtract
+# * multiply
+# / standard division 
+# // integer division
+# % Modulo operation (a form of division)
+
+#Standard division /
+# 7/2 = 3.5
+# always produces a FLOAT #
+# for example 10/2 = 5.0 not 5
+
+# Integer division //
+# Will always return an integer
+# for example 6//2 = 3 not 3.0
+#7//2 = 3 not 3.5
+# This type of division will always return the lowest whole number 
+
+#Modulo divison %
+#This is solving for the remainder
+# 6 % 2 = 0 because 6 = 2 * 3 + 0
+# 7 % 2 = 1 because 7 = 2 * 3 + 1
+
+# KEEP IN MIND THAT YOU CANNOT DIVIDE BY 0 WITH ANY OF THE OPERATORS /, //, % YOU WILL RECIEVE A "ZeroDivisionError"
+
+# Power operator (**)
+#2 ** 3 = 8 because 2^3 = 8 and because 2 * 2 * 2 = 8
+# If you use 2 integers for +, -, *, ** you will get back an integer but if you use 1 float or more you will get back a float
+# for example 3 + 2 = 5 but 3 + 2.0 = 5.0 and 3.0 + 2.0 = 5.0 as well
