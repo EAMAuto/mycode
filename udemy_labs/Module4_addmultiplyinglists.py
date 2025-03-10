dd542ce48e0189a65b563976c4256abb73b50c46
erika <erika@DESKTOP-B4NK1H7>
2025-03-10 14:29:39 -0400

    module 4 commit

---
+++ udemy_labs/Module4_addmultiplyinglists.py
@@ -0,0 +1,10 @@
+list_us = ['New York City', 'Chicago']
+list_uk = ['London', 'Bristol']
+list_all = list_us + list_uk
+print(list_all)
+#Output:
+['New York City', 'Chicago', 'London', 'Bristol']
+
+list_numbers = [0, 1] * 10
+#output:
+[0, 1, 0, 1, 0, 1, 0, 1, 0, 1,0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
